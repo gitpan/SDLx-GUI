2442914b67e3956c5f17b8eba0fabc8af5218e9b
Jerome Quelin <jquelin@cpan.org>
2013-02-26 14:17:19 +0000

    Import of JQUELIN/SDLx-GUI-0.001 from CPAN.
    gitpan-cpan-distribution: SDLx-GUI
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         JQUELIN/SDLx-GUI-0.001.tar.gz
    gitpan-cpan-author:       JQUELIN
    gitpan-cpan-maturity:     released

---
+++ lib/SDLx/GUI/Types.pm
@@ -0,0 +1,61 @@
+#
+# This file is part of SDLx-GUI
+#
+# This software is copyright (c) 2013 by Jerome Quelin.
+#
+# This is free software; you can redistribute it and/or modify it under
+# the same terms as the Perl 5 programming language system itself.
+#
+use 5.016;
+use warnings;
+
+package SDLx::GUI::Types;
+{
+  $SDLx::GUI::Types::VERSION = '0.001';
+}
+# ABSTRACT: Types used in the distribution
+
+use Moose::Util::TypeConstraints;
+
+enum 'PackSide' => qw{ top bottom left right };
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+SDLx::GUI::Types - Types used in the distribution
+
+=head1 VERSION
+
+version 0.001
+
+=head1 DESCRIPTION
+
+This module implements the specific types used by the distribution, and
+exports them (exporting is done by L<Moose::Util::TypeConstraints>).
+
+Current types defined:
+
+=over 4
+
+=item * PackSide - a simple enumeration, allowing C<top>, C<bottom>,
+C<left> and C<right>.
+
+=back
+
+=head1 AUTHOR
+
+Jerome Quelin <jquelin@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Jerome Quelin.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
